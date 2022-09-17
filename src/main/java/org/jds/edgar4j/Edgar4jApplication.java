9d432022f3f9a2a13e3af824863f643fa0970b95
Daniel Sobrado <daniel@danielsobrado.com>
2022-09-17 10:30:02 +0400

    Initial Commit

---
+++ src/main/java/org/jds/edgar4j/Edgar4jApplication.java
@@ -0,0 +1,13 @@
+package org.jds.edgar4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edgar4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edgar4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jds/edgar4j/Edgar4jApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jds.edgar4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edgar4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
