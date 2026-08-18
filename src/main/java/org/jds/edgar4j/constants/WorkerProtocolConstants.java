2e9d86f04ea7be549491c2e8538102fb4b28ec5e
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 14:23:31 +0400

    feat(workers): add coordination constants

--- src/main/java/org/jds/edgar4j/constants/WorkerProtocolConstants.java
+++ src/main/java/org/jds/edgar4j/constants/WorkerProtocolConstants.java
@@ -7,6 +7,8 @@ public final class WorkerProtocolConstants {
     public static final int MAX_CLIENT_VERSION_LENGTH = 64;
     public static final int MAX_DIAGNOSTIC_MESSAGE_LENGTH = 512;
     public static final int MAX_RESOURCE_ID_LENGTH = 512;
+    public static final int TOKEN_BYTES = 32;
+    public static final int LEASE_CANDIDATE_SCAN_LIMIT = 64;
 
     private WorkerProtocolConstants() {
     }
