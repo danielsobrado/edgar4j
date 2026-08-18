bcf7fe7fa121b647fbf76496493119025ecdfb62
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 15:03:38 +0400

    feat(workers): add streaming ingress directory

--- src/main/java/org/jds/edgar4j/constants/WorkerStorageConstants.java
+++ src/main/java/org/jds/edgar4j/constants/WorkerStorageConstants.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public final class WorkerStorageConstants {
 
     public static final String ARTIFACT_ROOT_DIRECTORY = "worker-artifacts";
+    public static final String INGRESS_DIRECTORY = "ingress";
     public static final String STAGING_DIRECTORY = "staging";
     public static final String VERIFIED_DIRECTORY = "verified";
     public static final String STAGING_SUFFIX = ".part";
