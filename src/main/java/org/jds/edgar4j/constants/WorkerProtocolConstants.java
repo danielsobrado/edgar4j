96ae76fe0fb6345794764838f09732a2f364ed79
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 14:14:47 +0400

    feat(workers): add protocol constants

---
+++ src/main/java/org/jds/edgar4j/constants/WorkerProtocolConstants.java
@@ -0,0 +1,13 @@
+package org.jds.edgar4j.constants;
+
+public final class WorkerProtocolConstants {
+
+    public static final int CURRENT_VERSION = 1;
+    public static final int MAX_TASKS_PER_LEASE_REQUEST = 16;
+    public static final int MAX_CLIENT_VERSION_LENGTH = 64;
+    public static final int MAX_DIAGNOSTIC_MESSAGE_LENGTH = 512;
+    public static final int MAX_RESOURCE_ID_LENGTH = 512;
+
+    private WorkerProtocolConstants() {
+    }
+}
