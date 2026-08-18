55fac4781919c3cc71b8fade7d0fdca6c48d5660
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 14:49:39 +0400

    feat(workers): define distributed resource ids

---
+++ src/main/java/org/jds/edgar4j/constants/WorkerResourceIds.java
@@ -0,0 +1,11 @@
+package org.jds.edgar4j.constants;
+
+public final class WorkerResourceIds {
+
+    public static final String COMPANY_TICKERS = "sec:company-tickers:";
+    public static final String COMPANY_TICKERS_EXCHANGES = "sec:company-tickers-exchanges:";
+    public static final String COMPANY_TICKERS_MUTUAL_FUNDS = "sec:company-tickers-mutual-funds:";
+
+    private WorkerResourceIds() {
+    }
+}
