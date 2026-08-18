8b5ba1b8583c8071a8a94ef506f8981dd5319c5b
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 14:49:31 +0400

    feat(workers): define task priorities

---
+++ src/main/java/org/jds/edgar4j/constants/WorkerTaskPriorities.java
@@ -0,0 +1,11 @@
+package org.jds.edgar4j.constants;
+
+public final class WorkerTaskPriorities {
+
+    public static final int BACKGROUND = 10;
+    public static final int NORMAL = 50;
+    public static final int INTERACTIVE = 100;
+
+    private WorkerTaskPriorities() {
+    }
+}
