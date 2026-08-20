3dcaa44d3274fd94ca749c2663082ea9b06afb41
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-20 10:34:01 +0400

    refactor(workers): separate lease validation from SEC permits

--- src/main/java/org/jds/edgar4j/service/WorkerSourceDispatchPolicy.java
+++ src/main/java/org/jds/edgar4j/service/WorkerSourceDispatchPolicy.java
@@ -5,4 +5,6 @@ import org.jds.edgar4j.model.WorkerSource;
 public interface WorkerSourceDispatchPolicy {
 
     void reserveRemoteDispatch(WorkerSource source);
+
+    void reserveSourceRequest(WorkerSource source);
 }
